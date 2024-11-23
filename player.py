19190072707dde95a0e44cd9e6c9d4ca97b9df47
Antoine Chen <102303534+Sachachen@users.noreply.github.com>
2024-11-23 22:23:46 +0000

    Sens unique

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player():
         self.current_room = None
     
     # Define the move method.
-    def move(self, direction,):
+    def move(self, direction):
         # Get the next room from the exits dictionary of the current room.
         next_room = self.current_room.exits[direction]
         # If the next room is None, print an error message and return False.
